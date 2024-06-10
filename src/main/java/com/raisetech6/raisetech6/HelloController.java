88682e654cd694dc365932e1410d6fe6efc5d1d4
star-field-jp <hoshigari728@gmail.com>
2024-06-10 13:53:23 +0900

    rename:#6 Changed filename HelloControler → HelloController.

--- src/main/java/com/raisetech6/raisetech6/HelloController.java
+++ src/main/java/com/raisetech6/raisetech6/HelloController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 
-public class HelloControler {
+public class HelloController {
 
     @GetMapping("hello")
     public String hello(){
