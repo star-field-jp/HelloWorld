e60364c12de2727f789e18cb2bc1d5b962d35c72
star-field-jp <hoshigari728@gmail.com>
2024-06-10 11:16:08 +0900

    firstcommit:#6 Project set up. q
    dq

---
+++ src/main/java/com/raisetech6/raisetech6/Raisetech6Application.java
@@ -0,0 +1,13 @@
+package com.raisetech6.raisetech6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Raisetech6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Raisetech6Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/raisetech6/raisetech6/Raisetech6ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raisetech6.raisetech6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Raisetech6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
